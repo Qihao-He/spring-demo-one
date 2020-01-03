6dc4851ea8ec1e4544807f1df910eeb6f067c36d
Qihao He <qi.he@maine.edu>
2020-01-02 20:35:19 -0500

    practice activity bean scopes with xml configuration

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -19,5 +19,12 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {
 		return "Just do it:" + fortuneService.getFortune();
 	}
-
+	
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method: doMyStartupStuff"); 
+	}
+	
+	public void doMyCleanupStuff() {
+		System.out.println("TrackCoach: inside method: doMyCleanupStuff"); 
+	}
 }
